9d3bb1f26b7d83a663d0890a46f6e6d933afc96c
Chinedu Arinze <chineduarinze@Chinedus-MacBook-Pro.local>
2019-01-23 10:28:33 -0500

    completed initial set up for application. Downloaded and configured necessary dependencies.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,4 @@
 // Global app controller
+import x from './test';
+const y = 23;
+console.log(`I imported ${x} from another module called test.js. And ${y} is from index.js`);
---
+++ src/js/test.js
@@ -0,0 +1,2 @@
+console.log('Imported Module');
+export default 2345346;
